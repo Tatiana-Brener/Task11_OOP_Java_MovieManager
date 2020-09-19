39fffc231d7173dc7db6e97b3d203c51561195ff
Tatana <burmatova@inbox.ru>
2020-09-19 18:13:21 +0300

    Data type of "id" fixed

--- src/main/java/ru/netology/domain/Movie.java
+++ src/main/java/ru/netology/domain/Movie.java
@@ -5,15 +5,12 @@ import lombok.Getter;
 @Getter
 
 public class Movie {
-    private String id;
+    private int id;
     private String imageUrl;
     private String name;
     private String genre;
 
-    public Movie(String id,
-                 String imageUrl,
-                 String name,
-                 String genre) {
+    public Movie(int id, String imageUrl, String name, String genre) {
         this.id = id;
         this.imageUrl = imageUrl;
         this.name = name;
