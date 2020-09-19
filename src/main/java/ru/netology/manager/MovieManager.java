dcc5484868b6def9a6fc2afee8fe1f90ee641de9
Tatana <burmatova@inbox.ru>
2020-09-19 18:15:52 +0300

    Fixed method getLastMovies, deleted method getAllMovies

--- src/main/java/ru/netology/manager/MovieManager.java
+++ src/main/java/ru/netology/manager/MovieManager.java
@@ -4,32 +4,31 @@ import ru.netology.domain.Movie;
 
 public class MovieManager {
     private Movie[] movies = new Movie[0];
+    private int afishaLenght = 10;
+
+    public MovieManager(int afishaLenght) {
+        this.afishaLenght = afishaLenght;
+    }
+
+    public MovieManager() {
+    }
 
     public void addMovie(Movie addedMovie) {
         int length = movies.length + 1;
         Movie[] tmp = new Movie[length];
-        System.arraycopy(movies, 0, tmp,
-                0, movies.length);
+        System.arraycopy(movies, 0, tmp, 0, movies.length);
 
         int lastMovieIndex = tmp.length - 1;
         tmp[lastMovieIndex] = addedMovie;
         movies = tmp;
     }
 
-    public Movie[] getAllMovies() {
-        Movie[] result = new Movie[movies.length];
+    public Movie[] getLastMovies() {
+        int resultLength = Math.min(afishaLenght, movies.length);
 
-        for (int i = 0; i < movies.length; i++) {
-            int index = movies.length - i - 1;
-            result[i] = movies[index];
-        }
-        return result;
-    }
+        Movie[] result = new Movie[resultLength];
 
-    public Movie[] getLastAddedMovies(int amountMovies) {
-        Movie[] result = new Movie[amountMovies];
-
-        for (int i = 0; i < amountMovies; i++) {
+        for (int i = 0; i < resultLength; i++) {
             int index = movies.length - i - 1;
             result[i] = movies[index];
         }
